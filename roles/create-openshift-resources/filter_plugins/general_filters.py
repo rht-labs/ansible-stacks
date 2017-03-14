d06e26127262abe30bd82a50809a09dde8d5cbcd
sherl0cks <justinmichaelholmes@gmail.com>
2017-03-14 11:25:15 -0600

    New oc syntax (#101)
    * adding new param filter
    
    * fixing issues with oc 3.4 deprecated syntax
    
    * adding quotes to env vars and updating tests
    
    * updating tests and filter for unquoted boolean in json

--- roles/create-openshift-resources/filter_plugins/general_filters.py
+++ roles/create-openshift-resources/filter_plugins/general_filters.py
@@ -10,9 +10,34 @@ def create_key_value_pairs_string(key_value_pairs):
             string += ','
     return string
 
+
+def create_param_string(key_value_pairs):
+    pairs = []
+    string = ""
+    for key in key_value_pairs:
+        pair = "\"{}={}\"".format(key, key_value_pairs[key])
+        pairs.append(pair)
+    for i, pair in enumerate(pairs):
+        string += " -p {}".format(pair)
+    return string
+
+
+def create_env_string(key_value_pairs):
+    pairs = []
+    string = ""
+    for key in key_value_pairs:
+        pair = "\"{}={}\"".format(key, key_value_pairs[key])
+        pairs.append(pair)
+    for i, pair in enumerate(pairs):
+        string += " -e {}".format(pair)
+    return string
+
+
 class FilterModule(object):
-    ''' A set of general filters to support OpenShift CLI'''
+    ''' A set of general filters to support OpenShift CLI '''
     def filters(self):
         return {
-            'key_value_pairs_string': create_key_value_pairs_string
+            'key_value_pairs_string': create_key_value_pairs_string,
+            'oc_param_string': create_param_string,
+            'oc_env_string': create_env_string
         }
